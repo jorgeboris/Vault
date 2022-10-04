55d9d89ebf406933ca5060794bbf4e4eb40f84f0
Jorge Vargas <eduardo.borisq@gmail.com>
2022-10-04 00:26:15 -0500

    Add methods new and create to Supervisor controller

--- app/controllers/supervisors_controller.rb
+++ app/controllers/supervisors_controller.rb
@@ -1,2 +1,11 @@
 class SupervisorsController < ApplicationController
+  def new
+    @supervisor = Supervisor.new
+    @supervisor.name = 'Demo'
+  end
+
+  def create
+    @supervisor = Supervisor.create(name: params[:supervisor][:name])
+    render json: @supervisor
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "bienvenida", to: "home#index"
+
+  get 'supervisors/new', to: 'supervisors#new'
+  post 'supervisors', to: 'supervisors#create'
 end
